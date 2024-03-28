0d615335a8927bf8c7664e93f4c842447632b6cf
Robin <rrlangton@gmail.com>
2024-03-27 22:11:33 -0500

    added logo.js

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # SVG-Logo-Maker
+
+## Description
+This purpose of this application is to create a simple logo for my projects. Using Node.js in the command-line, this application will generate a custom logo based on the user's choices.
+
+In the command-line, users will be prompted to reply to the following: 
+
+        Enter text less than 3 characters:
+        Enter a color for the text:
+        Chose a shape: circle, triangle, square
+        Enter a color for the shape:
+
+## Installation
+To install, you should clone this repo to your local device. Then, navigate to that repository and install the necessary packages using "npm install && npm start" in the command-line in, the terminal. Now, you are ready to run the program and generate a custom logo. 
+
+## Sources
+28-Stu_Mini-Project, 
+22-Stu_Promise-Chain, 
+07-Ins_Inheritance, 
+Bootcamp Xpert Learning Assistant, 
+Canvas tutors
--- index.js
+++ index.js
@@ -1,5 +1,54 @@
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer');
+const fs = require("fs")
+const Logo = require("./lib/logo")
+const Shape = require("./lib/shapes")
+const Text = require("./lib/text")
 
-const cli = new CLI();
 
-cli.run();
+inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter text less than 3 characters',
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter a color for the text',
+        },
+        {
+          type: 'list',
+          name: 'logoShape',
+          message: 'Chose a shape:',
+          choices: ['circle', 'triangle', 'square']
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter a color for the shape',
+        }
+      ])
+
+      .then((answers) => { 
+            console.log(answers);
+
+            const newShape = new Shape(answers.shapeColor);
+            
+            const newText = new Text(answers.text, answers.textColor)
+            
+            const newLogo1 = new Logo(newText, newShape);
+            // const newLogo2 = new Logo("XYZ", "yellow", "circle", "green");
+
+            const logoCode = (newLogo1.renderCode())
+
+            console.log(logoCode)
+            // console.log(newLogo2.renderCode())
+
+            fs.writeFile("./examples/result.svg", logoCode)
+
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log('Oops. Something went wrong.');
+      });
