9f23500d3980f9efa84dd278a6ffd28fc270ba18
Robin <rrlangton@gmail.com>
2024-04-06 21:07:37 -0500

    debugged index.js

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 const inquirer = require('inquirer');
-const fs = require("fs")
-const Logo = require("./lib/logo")
-const Circle = require("./lib/shapes")
-const Triangle = require("./lib/shapes")
-const Square = require("./lib/shapes")
-const Text = require("./lib/text")
+const fs = require("fs");
+const Logo = require("./lib/logo");
+const Circle = require("./lib/shapes");
+const Triangle = require("./lib/shapes");
+const Square = require("./lib/shapes");
+const Text = require("./lib/text");
 
-inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter text less than 4 characters',
-          validate: function(value) {
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text less than 4 characters',
+        validate: function(value) {
             if (value.length <= 4) {
-              return true;
+                return true;
             }
-            return 'Please enter text with less than 4 characters.';    
-        }},
-        {
-          type: 'input',
-          name: 'textColor',
-          message: 'Enter a color for the text',
-        },
-        {
-          type: 'list',
-          name: 'logoShape',
-          message: 'Chose a shape:',
-          choices: ['circle', 'triangle', 'square']
-        },
-        {
-          type: 'input',
-          name: 'shapeColor',
-        message: 'Enter a color for the shape',
+            return 'Please enter text with less than 4 characters.';
         }
-      ])
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for the text',
+    },
+    {
+        type: 'list',
+        name: 'logoShape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for the shape',
+    }
+])
+.then((answers) => {
+    console.log(answers);
+    let newShape;
+    switch (answers.logoShape) {
+        case "circle":
+            newShape = new Circle(answers.shapeColor);
+            break;
+        case "triangle":
+            newShape = new Triangle(answers.shapeColor);
+            break;
+        case "square":
+            newShape = new Square(answers.shapeColor);
+            break;
+        default:
+            console.log("Invalid shape selection.");
+    }
 
-      .then((answers) => { 
-            console.log(answers);
+    const newText = new Text(answers.text, answers.textColor);
+    const newLogo1 = new Logo(newText, newShape);
+    const logoCode = newLogo1.renderCode();
+    console.log(logoCode);
 
-            let newShape;
-            switch (answers.logoShape) {
-              case "circle":
-                newShape = new Circle(answers.shapeColor);
-                break;
-              case "triangle":
-                newShape = new Triangle(answers.shapeColor);
-                break;
-              case "square":
-                newShape = new Square(answers.shapeColor);
-                break;
-              default:
-                console.log("Invalid shape selection.");
-            }
-
-            const newText = new Text(answers.text, answers.textColor)
-            
-            const newLogo1 = new Logo(newText, newShape);
-            // const newLogo2 = new Logo("XYZ", "yellow", "circle", "green");
-
-            const logoCode = (newLogo1.renderCode())
-
-            console.log(logoCode)
-            // console.log(newLogo2.renderCode())
-
-            fs.writeFile("./examples/result.svg", logoCode)
-          })
-
-      .catch((err) => {
-        console.log(err);
-        console.log('Oops. Something went wrong.');
-      });
+    fs.writeFile("./examples/result.svg", logoCode, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('File written successfully.');
+        }
+    });
+})
+.catch((err) => {
+    console.log(err);
+    console.log('Oops. Something went wrong.');
+});
